4f1aef4c6c05c7a342848f32b5686c883b4dd676
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2022-03-08 12:14:18 +0300

    h

--- server/server.js
+++ server/server.js
@@ -3,6 +3,24 @@ const http = require('http');
 const { v4: uuidv4 } = require('uuid')
 const cors = require('cors');
 const twilio = require('twilio');
-require('dotenv').config()
 
-PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.use(cors());
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
+
+
+
+server.listen(PORT, () => {
+  console.log('Running on Port 3000')
+})
